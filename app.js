20355484af10d9704dc46dc305f70e634f716f0b
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2020-03-22 15:45:18 +0100

    installation and use handlebars for handle the 404 error page

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const rootDir = require('./util/path');
 const path = require('path');
-const express = require('express');
 
-const app = express();
+const express = require('express');
 const bodyParser = require('body-parser')
+const expressHbs = require('express-handlebars');
+const app = express();
+
+
+
+app.engine('hbs',  expressHbs({
+    extname: "hbs",
+    defaultLayout: "",
+    layoutsDir: "",
+ }));
+ 
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded());
 
@@ -13,6 +26,7 @@ const shopRoutes = require('./routes/shop');
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views/404.html'));
+    res.status(404).render('404', { pageTitle: 'Page Not Found'});
 })
+
 app.listen(3000);
